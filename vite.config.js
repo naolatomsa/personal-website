93de66724036affcfced1871cb8701a9bce2358c
naolatomsa <naolatomsa4@gmail.com>
2025-07-24 14:46:20 +0300

    live

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    server: {
-    host: "192.168.43.18",
+  base: "personal-website",
+  server: {
+    // host: "192.168.43.18",
     // host: "192.168.8.154",
     // host:'192.168.8.106',
-    port: 3000,
+    // port: 3000,
     // open: true,
   },
 });
